9e623e38793e873a852d5aa175021ce99e6880b0
FingerDemon <RichWard@cox.net>
2012-04-02 15:54:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spellem::Application.config.secret_token = '1968275d2d08d16fcb6da4758d936f23eccd57260f6f51ed94c34236e45d14bcb99ba91a30057e2de40f1fec91c4634939753b5b06a30b266773ce251c6031b4'
